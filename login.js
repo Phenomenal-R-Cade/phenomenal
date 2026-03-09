cea70119c4b92f0b0b3c2d3f4347e1ffd0ae3a42
Phenomenal-R-Cade <phenomenal.r.cade@gmail.com>
2026-03-09 12:02:08 -0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+cconst gameText = document.getElementById('game-text2');
+const userInput = document.getElementById('user-input2');
+const submitBtn = document.getElementById('submit-btn2');
+function print(text) {
+  game Text.innerText +=text + "\n";
+  gameText.scrollTop = gameText.scrollheight;
+}
